7f68605e58b385f42b2d371b9cc4f5889c424b2c
lions1999 <57173336+lions1999@users.noreply.github.com>
2022-10-17 09:37:44 +0200

    dao

--- Condominium/src/logic/engineeringclasses/dao/SqlDAO.java
+++ Condominium/src/logic/engineeringclasses/dao/SqlDAO.java
@@ -5,10 +5,9 @@ import java.sql.*;
 public class SqlDAO {
 
 	private static final String URL = "jdbc:mysql://localhost:3306/condominium_db";
-	/*
+
 	private static final String USER = "condominium";
 	private static final String PASSWORD = "ispw2223";
-	*/
 
 	private Connection conn;
 
@@ -23,13 +22,13 @@ public class SqlDAO {
 	
 	void connect() throws SQLException 	{
 		 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-		 conn = DriverManager.getConnection(URL,"condominium","ispw2223");
+		 conn = DriverManager.getConnection(URL,USER,PASSWORD);
 		 stmt = conn.createStatement();
 	}
 	
 	PreparedStatement prepConnect(String sql) throws SQLException{
 		 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-		 conn = DriverManager.getConnection(URL,"condominium","ispw2223");
+		 conn = DriverManager.getConnection(URL,USER,PASSWORD);
 		 return conn.prepareStatement(sql);
 	}
 	
